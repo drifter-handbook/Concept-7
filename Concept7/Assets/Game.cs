2f819d095b3efedad48e361cbfbeb580ae916443
Savannah Mann <mannnnnn@users.noreply.github.com>
2022-05-30 16:47:59 -0500

    Hooking up some elements into the Game Screen
    Sneaking in my inspector work before someone else does

--- Concept7/Assets/Game.cs
+++ Concept7/Assets/Game.cs
@@ -14,22 +14,24 @@ public class Game : MonoBehaviour
 
     [Header("Point Information")]
     public int PointsPerEnemy = 10;
-    public float TargetTime = 5000;
-    private float TimeElapsed;
-    private int EnemiesKilled;
-    private int Points;
+    public float MaxTimePoints = 180;
+    [HideInInspector] public float TimeElapsed;
+    [HideInInspector] public float  EnemiesKilled;
+    [HideInInspector] public float  Points;
 
     [Header("UI")]
-    public GameScreen gameScreen;
+    public GameScreen GameScreen;
 
     private GameState gameState = GameState.PLAYING;
 
     public void Start(){
         CurrentLives = StartingLives;
         TimeElapsed = 0;
+        GameScreen.Game = this;
     }
 
     public void Update(){
+        if(gameState == GameState.ENDED) return;
         if(gameState == GameState.PLAYING){
             TimeElapsed += Time.deltaTime;
         }
@@ -46,16 +48,19 @@ public class Game : MonoBehaviour
         if(CurrentLives <= 0){
             EndGame(false);
         }
+        GameScreen.UpdateLives();
     }
 
     public void EndGame(bool wasWin){
-        gameState = wasWin ? GameState.VICTORY : GameState.DEFEAT;
+        gameState = GameState.ENDED;
+        Points = Points += (MaxTimePoints -= TimeElapsed);
+        GameScreen.ShowEndScreen(wasWin);
     }
+    
 
     public enum GameState{
         PLAYING,
-        VICTORY,
-        DEFEAT,
+        ENDED,
         PAUSED
     }
 
